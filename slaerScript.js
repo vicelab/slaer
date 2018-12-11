6f8dbf9293530e5583065ba292771d20c06ee66e
Denis Vashchenko <denisvashchenko@gmail.com>
2018-12-10 21:14:03 -0800

    beginning visualization of individual crops

--- slaerScript.js
+++ slaerScript.js
@@ -3,11 +3,12 @@
 var fc = ee.FeatureCollection(table).select("Crop2014")
 
 //Removes the urban enviroments from the map and dataset
-var wantedLand = fc.filter(ee.Filter.neq("Crop2014", "Urban"))
+var nonUrban = fc.filter(ee.Filter.neq("Crop2014", "Urban"))
+var almonds = fc.filter(ee.Filter.eq("Crop2014", "Almonds"))
 
-var colors = [
-  //Add colors for different types of crops
-];
 
-Map.addLayer(wantedLand, {palette: colors})
-Map.setCenter(-120.98891 , 37.6617049, 7)
+var colors = [];
+
+Map.addLayer(nonUrban, {palette: colors})
+Map.addLayer(almonds.draw({color: '8B4513', strokeWidth: 2}), {}, 'drawn')
+Map.setCenter(-120.98891 , 37.6617049, 12)
